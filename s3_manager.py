67e5ec00948d5d710c0ecce3d93c80eef28b8d1a
Julio Menezes <julincpm09@gmail.com>
2026-03-31 16:32:02 -0300

    Mudança aprimoramento de segurança

--- s3_manager.py
+++ s3_manager.py
@@ -119,3 +119,4 @@ class S3Manager:
             return False, "Credenciais AWS não encontradas. Rode 'aws configure'."
         except ClientError as erro:
             return False, f"Erro ao deletar arquivo: {erro}"
+        
